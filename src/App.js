9e639b0196b8b3561a221b6b3708c64fb198c364
Byakuya91 <nganti91@yahoo.com>
2022-08-11 17:45:22 -0400

    I successfully crafted the HTML for the calculator and laid the groundwork for using CSS grid for the calculator. I'll be in the process of researching useReducer for the calculations of the calculator itself and how to go about it.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from "react";
 
 function App() {
-  return <h1>Test text</h1>;
+  return (
+    <div className="calc-grid">
+      {/* Calculator display */}
+      <div className="calc-output">
+        <div className="calc-previous-operand"></div>
+        <div className="calc-current-operand"></div>
+      </div>
+      {/* Calculator buttons */}
+      <button className="calc-span-two">AC</button>
+      <button>DEL</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="calc-span-two"> = </button>
+    </div>
+  );
 }
 
 export default App;
