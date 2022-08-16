4fc11bcdb078f0a646ff20c1ea2fdc2314a13a50
Byakuya91 <nganti91@yahoo.com>
2022-08-16 15:13:30 -0400

    I've done the research on Redcuer and done the following. Created components for the digit and operation buttons and utilized Reducer to manage and update the pieces of state within the calculator itself. There are still a load of problems within the calculator, namely the operation buttons not working and I'm unable to create decimal numbers.  This will need to be debugged/ worked on promptly. Objectives next will be to work on fixing those elements I mentioned above and to fill out the rest of the switch case for the dispatch function.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,77 @@
-import React from "react";
+import React, { useReducer } from "react";
+import DigitButton from "./Components/DigitButton/DigitButton";
+import OperationButton from "./Components/OperationButton/OperationButton";
 import "./styles.css";
+
+// GLOBAL VARIABLES (DEFINE ACTIONS THAT CAN BE TAKEN WITH A CALCULATOR)
+export const ACTIONS = {
+  CHOOSE_OPERATION: "choose-operation",
+  ADD_DIGIT: "add-digit",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUTE: "evalute",
+};
+
+// Reducer function for calculator
+// action will be broken down into type and payload
+function reducer(state, { type, payload }) {
+  // switch case to organize each of the actions depicted above
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      // checking for edge cases
+      if (payload.digit === "0" && state.currentOperand === "0") return state;
+      if (payload.digit === "." && state.currentOperand.includes("."))
+        return state;
+      return {
+        ...state,
+        currentOperand: payload.digit,
+      };
+  }
+}
+
 function App() {
+  // TODOS (DONE)
+  //  Investigate UseReducer and incorporate it into calculator design.
+  //  Investigate other projects and how they utilize it.
+  // implement your own solution into it
+  //   Calculator functions:
+  //  previous operand and current operand must do some calculation
+  // or operands are modified before calculation
+  // reducer Actions can boil down to these elements.
+
+  // UseReducer hook
+  const [{ currentOperand, previousOperand, Operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
+
   return (
     <div className="calc-grid">
       {/* Calculator display */}
       <div className="calc-output">
-        <div className="calc-previous-operand">50,000 *</div>
-        <div className="calc-current-operand">50,000</div>
+        <div className="calc-previous-operand">
+          {previousOperand} {Operation}
+        </div>
+        <div className="calc-current-operand">{currentOperand}</div>
       </div>
       {/* Calculator buttons */}
       <button className="calc-span-two">AC</button>
       <button>DEL</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
+      <OperationButton operation="÷" dispatch={dispatch} />
+      <DigitButton digit="1" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
+      <OperationButton operation="*" dispatch={dispatch} />
+      <DigitButton digit="4" dispatch={dispatch} />
+      <DigitButton digit="5" dispatch={dispatch} />
+      <DigitButton digit="6" dispatch={dispatch} />
+      <OperationButton operation="+" dispatch={dispatch} />
+      <DigitButton digit="7" dispatch={dispatch} />
+      <DigitButton digit="8" dispatch={dispatch} />
+      <DigitButton digit="9" dispatch={dispatch} />
+      <OperationButton operation="-" dispatch={dispatch} />
+      <DigitButton digit="." dispatch={dispatch} />
+      <DigitButton digit="0" dispatch={dispatch} />
       <button className="calc-span-two"> = </button>
     </div>
   );
