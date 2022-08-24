9ca0d23e1bfe21cc49c2504b43e71f61e2e35142
Byakuya91 <nganti91@yahoo.com>
2022-08-24 09:10:20 -0400

    I've finally fixed the issue of allowing multiple digits. The problem was constrained to the add Digits area in the switch case which needed to be retooled with regards to allowing multiple digits to happen. Overall, the calculator is fully functional and completed. I may attempt to add a square route button/ squared function which should just require adding to the evaluate function but the CSS will definitely need to be retoold.  PROJECT IS COMPLETED.

--- src/App.js
+++ src/App.js
@@ -18,19 +18,53 @@ function reducer(state, { type, payload }) {
   // switch case to organize each of the actions depicted above
   switch (type) {
     case ACTIONS.ADD_DIGIT:
-      // checking for edge cases
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false,
+        };
+      }
+      // checking for edge cases with 0. Making sure if 0 is on screen, more zeros can't be added, unless the decimal is added.
       if (payload.digit === "0" && state.currentOperand === "0") {
         return state;
       }
-
-      if (payload.digit === "." && state.currentOperand.includes("."))
+      // checking for edge cases with decimal points so no multiple decimal points are displayed.
+      if (payload.digit === "." && state.currentOperand.includes(".")) {
         return state;
+      }
+      //  showcasing the current state and the digits.
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+      };
+    case ACTIONS.CHOOSE_OPERATION:
+      if (state.currentOperand == null && state.previousOperand == null) {
+        return state;
+      }
+
+      if (state.currentOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+        };
+      }
+
+      if (state.previousOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null,
+        };
+      }
 
       return {
         ...state,
-        currentOperand: payload.digit,
+        previousOperand: evaluate(state),
+        operation: payload.operation,
+        currentOperand: null,
       };
-
     // actions: choosing an operation
     case ACTIONS.CHOOSE_OPERATION:
       if (state.currentOperand == null && state.previousOperand == null) {
@@ -65,6 +99,48 @@ function reducer(state, { type, payload }) {
     //  action to clear the digits
     case ACTIONS.CLEAR:
       return {};
+    // case for deleting digits
+    case ACTIONS.DELETE_DIGIT:
+      //  checking if the state is in overwrite
+      if (state.overwrite) {
+        return {
+          ...state,
+          overwrite: false,
+          currentOperand: null,
+        };
+      }
+      // checking if current operand is null
+      if (state.currentOperand == null) {
+        return state;
+      }
+      if (state.currentOperand.length === 1) {
+        return { ...state, currentOperand: null };
+      }
+
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1),
+      };
+
+    //  Using the equal button for calculations
+    case ACTIONS.EVALUTE:
+      // checking if we have all the values inside the calculator. If we do not, it just gives us the state
+      if (
+        state.operation == null ||
+        state.currentOperand == null ||
+        state.previousOperand == null
+      ) {
+        return state;
+      }
+
+      // if we have the proper pieces of state inside the calculator
+      return {
+        ...state,
+        overwrite: true,
+        previousOperand: null,
+        operation: null,
+        currentOperand: evaluate(state),
+      };
   }
 }
 
@@ -98,18 +174,24 @@ function evaluate({ currentOperand, previousOperand, operation }) {
   return computation.toString();
 }
 
-function App() {
-  // TODOS (DONE)
-  //  Investigate UseReducer and incorporate it into calculator design.
-  //  Investigate other projects and how they utilize it.
-  // implement your own solution into it
-  //   Calculator functions:
-  //  previous operand and current operand must do some calculation
-  // or operands are modified before calculation
-  // reducer Actions can boil down to these elements.
+// integer formatter for calculation
 
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
+  maximumFractionDigits: 0,
+});
+
+// function to format the operand
+function formatOperand(operand) {
+  // checking if an operand exists
+  if (operand == null) return;
+  const [integer, decimal] = operand.split(".");
+  if (decimal == null) return INTEGER_FORMATTER.format(integer);
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`;
+}
+
+function App() {
   // UseReducer hook
-  const [{ currentOperand, previousOperand, Operation }, dispatch] = useReducer(
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
     reducer,
     {}
   );
@@ -119,9 +201,11 @@ function App() {
       {/* Calculator display */}
       <div className="calc-output">
         <div className="calc-previous-operand">
-          {previousOperand} {Operation}
+          {formatOperand(previousOperand)} {operation}
         </div>
-        <div className="calc-current-operand">{currentOperand}</div>
+        <div className="calc-current-operand">
+          {formatOperand(currentOperand)}
+        </div>
       </div>
       {/* Calculator buttons */}
       <button
@@ -130,7 +214,9 @@ function App() {
       >
         AC
       </button>
-      <button>DEL</button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
+        DEL
+      </button>
       <OperationButton operation="÷" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
@@ -146,7 +232,13 @@ function App() {
       <OperationButton operation="-" dispatch={dispatch} />
       <DigitButton digit="." dispatch={dispatch} />
       <DigitButton digit="0" dispatch={dispatch} />
-      <button className="calc-span-two"> = </button>
+      <button
+        className="calc-span-two"
+        onClick={() => dispatch({ type: ACTIONS.EVALUTE })}
+      >
+        {" "}
+        ={" "}
+      </button>
     </div>
   );
 }
